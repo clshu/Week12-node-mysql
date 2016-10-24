ba58f3f1af157b2793a67157e6a50f486c149e23
clshu <ccss0630@yahoo.com>
2016-10-24 16:43:00 -0500

    Change Electronic to Electronics, add SQL commands to CREATE TABLE Departments, add LOAD DATA command to load data file departments.csv

--- config.sql
+++ config.sql
@@ -16,3 +16,18 @@ INTO TABLE Products
 FIELDS TERMINATED BY  ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
+
+CREATE TABLE Departments (
+	department_id INT NOT NULL AUTO_INCREMENT,
+	department_name VARCHAR(20),
+	overhead_cost DECIMAL(10,2) DEFAULT 0.00,
+	total_sales DECIMAL(20,2) DEFAULT 0.00,
+	PRIMARY KEY(department_id)
+) AUTO_INCREMENT = 1000;
+
+LOAD DATA INFILE '/Users/chinlong/Desktop/Bootcamp/HW/Week12-node-mysql/departments.csv'
+INTO TABLE Departments
+FIELDS TERMINATED BY  ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
+
